89847858a84f669bb722b9d91951ca630ff7834d
Poonam Sharma <ppoonamsharma285@gmail.com>
2025-06-25 19:45:59 +0530

    Add files via upload

---
+++ demo_loan.py
@@ -0,0 +1,38 @@
+# for approved
+{
+  "Gender": 1,
+  "Age": 20.67,
+  "Debt": 5.29,
+  "Married": 1,
+  "BankCustomer": 1,
+  "Industry": 2,
+  "Ethnicity": 1,
+  "YearsEmployed": 0.375,
+  "PriorDefault": 1,
+  "Employed": 1,
+  "CreditScore": 1,
+  "DriversLicense": 0,
+  "Citizen": 2,
+  "ZipCode": 160,
+  "Income": 25000
+}
+
+# for not approved 
+
+{
+  "Gender": 0,
+  "Age": 19.17,
+  "Debt": 0.585,
+  "Married": 0,
+  "BankCustomer": 0,
+  "Industry": 3,
+  "Ethnicity": 1,
+  "YearsEmployed": 0.585,
+  "PriorDefault": 1,
+  "Employed": 0,
+  "CreditScore": 0,
+  "DriversLicense": 1,
+  "Citizen": 2,
+  "ZipCode": 160,
+  "Income": 0
+}
